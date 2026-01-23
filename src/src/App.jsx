51973153035f85d5bee43d171726d3f72cc8777d
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2026-01-23 11:44:35 -0800

    Enable Routing Functionality in Application by Adding React-Router

---
+++ src/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+import Login from './pages/Login/Login.jsx'
+import Dashboard from './pages/Dashboard/Dashboard.jsx'
+import Note from './pages/Note/Note.jsx'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/note" element={<Note />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
