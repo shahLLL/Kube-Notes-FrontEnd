69a2326ac0822ad69212a47599ee0b5ecbebd2d2
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2026-01-25 09:07:57 -0800

    Remove redundant import

--- src/src/pages/Login/Login.jsx
+++ src/src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { registerUser, autheticateUser } from '../../services/authServices';
 import logo from '../../assets/KubeNotes_Logo.png'
