4e054d606f82178da9b290f60c2a5ffe8aed94cb
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2026-01-25 15:48:56 -0800

    Add styling changes

--- src/src/pages/Login/Login.jsx
+++ src/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { registerUser, autheticateUser } from '../../services/authServices';
+import { registerUser, autheticateUser } from '../../services/authServices'
 import logo from '../../assets/KubeNotes_Logo.png'
 import './Login.css'
 
